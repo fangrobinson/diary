17b36b74282824f7c2b4ecffb2787e642fa7559b
Robinson <fang.robinson@gmail.com>
2020-06-09 17:32:29 -0300

    fixed vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/scrollable-text-experience/'
-        : '/'
+  "publicPath": "/",
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }

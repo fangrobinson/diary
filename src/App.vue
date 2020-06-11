5a0c30317296ba9ea28b24591ec7d97fe5194709
Robinson <fang.robinson@gmail.com>
2020-06-11 16:15:33 -0300

    ADD new MyMenu Component

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,12 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-navigation-drawer app v-model="drawer" class="blue-grey lighten-3" right="true">
+    <v-navigation-drawer app v-model="drawer" class="blue-grey lighten-3" right="true" temporary>
       <p>test</p>
     </v-navigation-drawer>
 
     <v-content>
+      <v-row align="center"><MyMenu></MyMenu></v-row>
       <HelloWorld/>
     </v-content>
   </v-app>
@@ -36,12 +37,14 @@
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import MyMenu from "./components/MyMenu";
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    MyMenu,
   },
 
   data: () => ({
---
+++ src/components/MyMenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-row align="center">
+        <v-menu
+                v-model="value"
+                :disabled="disabled"
+                :absolute="absolute"
+                :open-on-hover="openOnHover"
+                :close-on-click="closeOnClick"
+                :close-on-content-click="closeOnContentClick"
+                :offset-x="offsetX"
+                :offset-y="offsetY"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                        color="primary"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
+                >
+                    Dropdown
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item
+                        v-for="(item, index) in items"
+                        :key="index"
+                >
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: [
+                { title: 'Click Me' },
+                { title: 'Click Me' },
+                { title: 'Click Me' },
+                { title: 'Click Me 2' },
+            ],
+            disabled: false,
+            absolute: false,
+            openOnHover: false,
+            value: false,
+            closeOnClick: true,
+            closeOnContentClick: true,
+            offsetX: false,
+            offsetY: true,
+        }),
+    }
+</script>

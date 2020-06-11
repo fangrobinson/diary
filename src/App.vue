6f8031a9a13eacb647772957dc9d8f864a00f6d5
Robinson <fang.robinson@gmail.com>
2020-06-11 15:30:37 -0300

    MODIFY App bar contents

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,18 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="blue-grey lighten-3"
+      dense="true"
       dark
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Robinson Icon"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://i.imgur.com/FJeOp8h.png"
           transition="scale-transition"
           width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
 

9044b7c446258001b27cb1f9be7fb3ee93204ef7
Robinson <fang.robinson@gmail.com>
2020-07-21 20:36:02 -0300

    EDIT deploy route

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:fang.robinson@gmail.com/scrollable-text-experience.git master:gh-pages
-
+# git push -f git@github.com:fang.robinson@gmail.com/scrollable-text-experience.git master:gh-pages
+git push -f git@github.com:fang.robinson@gmail.com/diary.git master:gh-pages
 cd -
